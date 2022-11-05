75e23154dde12809637804ad6eb959d1f4e82811
Shreyas <gopnarayanshreyas2004@gmail.com>
2022-11-05 11:43:55 +0530

    Priyanka second

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,15 +7,7 @@ const Navbar = () => {
                 <a className="navbar-brand" href="#">
                     Apna Cart
                 </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
